e539bdb24261eb07448ddbe1449e624bb47e6044
Sean Farr <seantf68@gmail.com>
2021-10-13 10:15:01 -0400

    modified Employee_Database_challenge.sql, added mentorship_eligibility.csv, and created resources folder for png files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -21,8 +21,7 @@ SELECT DISTINCT ON (emp_no) emp_no,
 	title
 INTO unique_titles
 FROM retirement_titles
-ORDER BY emp_no, to_date DESC
-;
+ORDER BY emp_no, to_date DESC;
 
 -- Retieve number of emp, by most recent job, about to retire
 
@@ -30,5 +29,23 @@ SELECT COUNT(emp_no), title
 INTO retiring_titles
 FROM unique_titles 
 GROUP BY title
-ORDER BY count DESC
-;
+ORDER BY count DESC;
+
+-- Query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+
+SELECT DISTINCT ON (e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title	
+INTO mentorship_eligibility
+FROM employees as e
+	INNER JOIN dept_employee as de
+	ON e.emp_no = de.emp_no
+	INNER JOIN titles as t
+	ON e.emp_no = t.emp_no
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
